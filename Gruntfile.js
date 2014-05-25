ee015a839f0aba14316d6db91ae542374ff5cd55
Owl <zly.ojciec@gmail.com>
2014-05-25 23:48:40 +0100

    Update beta.2.0

---
+++ Gruntfile.js
@@ -0,0 +1,275 @@
+/*
+ * Owl Carousel 
+ * 
+ * Bartosz Wojciechowski
+ * 
+ * Copyright (c) 2014
+ * Licensed under the MIT license.
+ */
+
+module.exports = function (grunt) {
+
+	if(!grunt.file.isDir('bower_components')) {
+		grunt.fail.fatal('>> Please run "bower install" before continuing.');
+	}
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		app: grunt.file.readJSON('_config.json'),
+		vendor: 'bower_components',
+		banner: 
+			'/**\n' +
+			' * Owl Carousel v<%= pkg.version %>\n' +
+			' * Copyright 2013-<%= grunt.template.today("yyyy") %> <%= pkg.author.name %>\n' +
+			' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+			' */\n',
+
+		// assemble
+		assemble: {
+			options: {
+				flatten: false,
+				expand: true,
+				production: false,
+				assets: '<%= app.docs.dest %>/assets',
+				postprocess: require('pretty'),
+
+				// Metadata
+				pkg: '<%= pkg %>',
+				app: '<%= app %>',
+				data: ['<%= app.docs.src %>/data/*.{json,yml}'],
+
+				// Templates
+				partials: '<%= app.docs.templates %>/partials/*.hbs',
+				layoutdir: '<%= app.docs.layouts %>/',
+
+				// Extensions
+				helpers: '<%= app.docs.src %>/helpers/*.js'
+			},
+			index: {
+				options:{
+					layout: 'home.hbs',
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= app.docs.pages %>/',
+					src: '*.hbs',
+					dest: '<%= app.docs.dest %>/'
+				}]
+			},
+			demos: {
+				options:{
+					layout: 'demos.hbs'
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= app.docs.pages %>/demos/',
+					src: '*.hbs',
+					dest: '<%= app.docs.dest %>/demos'
+				}]
+			},
+			docs: {
+				options:{
+					layout: 'docs.hbs'
+				},
+				files: [{
+					expand: true,
+					cwd: '<%= app.docs.pages %>/docs/',
+					src: '*.hbs',
+					dest: '<%= app.docs.dest %>/docs'
+				}]
+			}
+		},
+		
+		// clean
+		clean: {
+			docs: ['<%= app.docs.dest %>/**/*.*'],
+			dist: ['dist/**/*.*']
+		},
+
+		// sass
+		sass: {
+			docs:{
+				options: {
+					style: 'compressed',
+					loadPath: [
+						'<%= app.docs.src %>/assets/scss/',
+						'bower_components/foundation/scss'
+					]
+				},
+				files: {
+					'<%= app.docs.dest %>/assets/css/docs.theme.min.css': '<%= app.docs.src %>/assets/scss/docs.theme.scss'
+				}
+			},
+			dist: {
+				options:{
+					style: 'expanded'
+				},
+				files:[{
+					expand: true,
+					flatten: true,
+					cwd: 'src/scss/',
+					src: '*scss',
+					dest: 'src/css/',
+					ext: '.css',
+					extDot: 'last'
+				}]
+			}
+		},
+
+		concat: {
+			dist:{
+				files: {
+					'dist/assets/owl.carousel.css': ['src/css/*.css','!src/css/owl.theme*.css'],
+					'dist/<%= pkg.name %>.js': '<%= app.src.scripts %>'
+				}
+			}
+		},
+
+		cssmin: {
+			dist: {
+				files: {
+					'dist/assets/<%= pkg.name %>.min.css': ['src/css/*.css','!src/css/owl.theme*.css'],
+					'dist/assets/owl.theme.default.min.css': 'src/css/owl.theme.default.css',
+					'dist/assets/owl.theme.green.min.css': 'src/css/owl.theme.green.css'
+				}
+			}
+		},
+
+		uglify: {
+			dist: {
+				files:{
+					'dist/<%= pkg.name %>.min.js': '<%= app.src.scripts %>'
+				}
+			}
+		},
+
+		// copy 
+		copy: {
+			themes:{
+				expand: true,
+				flatten: true,
+				cwd: 'src/css/',
+				src: [
+					'owl.theme.*'
+				],
+				dest: 'dist/assets'
+			},
+			distImages:{
+				expand: true,
+				flatten: true,
+				cwd: 'src/',
+				src: [
+					'img/*.*'
+				],
+				dest: 'dist/assets'
+			},
+
+			distToDocs: {
+				expand: true,
+				cwd: 'dist/',
+				src: [
+					'**/*.*'
+				],
+				dest: '<%= app.docs.dest %>/assets/owlcarousel'
+			},
+
+			docsAssets: {
+				expand: true,
+				cwd: '<%= app.docs.src %>/assets/',
+				src: [
+					'css/*.css',
+					'vendors/*.js',
+					'vendors/*.map',
+					'img/*.*',
+					'js/*.*'
+				],
+				dest: '<%= app.docs.dest %>/assets/'
+			},		
+			readme: {
+				files: [{
+					'dist/LICENSE-MIT' : 'LICENSE-MIT',
+					'dist/README.md' : 'README.md'
+				}]
+			}
+		},
+
+		// Connect 
+		connect: {
+			options: {
+				port: 9000,
+				open: true,
+				livereload: true,
+				hostname: 'localhost'				
+			},
+			docs: {
+				options: {
+					base: "<%= app.docs.dest %>"
+				}
+			}
+		},
+
+		// Watch
+		watch: {
+			options: {
+				livereload: true
+			},
+			templates: {
+				files: ['<%= app.docs.templates %>/**/*.hbs'],
+				tasks: ['assemble']
+			},
+			sass :{
+				files: ['<%= app.docs.src %>/assets/**/*.scss'],
+				tasks: ['sass:docs']
+			},
+			sassDist: {
+				files: ['src/**/*.scss'],
+				tasks: [ 'sass:dist', 'concat:dist', 'cssmin:dist', 'copy:themes']
+			},
+			jsDocs: {
+				files: ['<%= app.docs.src %>/assets/**/*.js'],
+				tasks: ['copy:docsAssets']
+			},
+			js :{
+				files: ['src/**/*.js'],
+				tasks: ['uglify:dist','concat:dist','copy:distToDocs']
+			},
+			helpers :{
+				files: ['<%= app.src %>/helpers/*.js'],
+				tasks: ['assemble']
+			}
+		},
+
+		jshint: {
+			all: ['src/js/*.js','*.js']
+		},
+
+		// compress zip
+		compress: {
+			zip: {
+				options: {
+					archive: 'download/<%= pkg.version %>/owl.carousel.zip'
+				},
+				files: [{
+					expand: true,
+					cwd: 'dist/',
+					src: ['**'],
+					dest: 'owl.carousel.<%= pkg.version %>'
+				}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('assemble');
+
+	// Tasks
+	grunt.registerTask('dist', ['clean:dist', 'sass:dist', 'concat:dist', 'cssmin:dist', 'copy:themes', 'copy:distImages', 'uglify:dist','copy:readme']);
+
+	grunt.registerTask('docs', ['clean:docs','assemble','sass:docs','copy:docsAssets','copy:distToDocs']);
+
+	grunt.registerTask('default', ['dist','docs']);
+	
+	grunt.registerTask('serve', ['connect:docs','watch']);
+	
+	grunt.registerTask('zip', ['compress']);
+};
---
+++ src/js/owl.video.js
@@ -0,0 +1,260 @@
+/**
+ * Video Plugin
+ * @since 2.0.0
+ */
+
+;(function ( $, window, document, undefined ) {
+
+	Video = function(scope){
+		this.owl = scope;
+		this.owl.options = $.extend({},Video.Defaults, this.owl.options);
+		
+		this.owl.dom.$el.on('click.owl.video', '.owl-video-play-icon', $.proxy(function(e) {
+			this.playVideo(e);
+		}, this));
+
+		this.owl.dom.$el.on({
+			'onResize.owl': $.proxy(function(e) {
+				if (this.owl.options.video && !this.isInFullScreen()) e.preventDefault();
+			}, this),
+			'onRefresh.owl onChanged.owl': $.proxy(function(e) {
+				if (this.owl.state.videoPlay) this.stopVideo();
+			}, this),
+			'onRefresh.owl': $.proxy(function(e) {
+				if(!this.owl.options.video) return false;
+				this.owl.dom.$el.one('onUpdated.owl', $.proxy(this.checkVideoLinks, this));
+			}, this)
+		});
+	};
+	
+	Video.Defaults = {
+		video:			false,
+		videoHeight:	false,
+		videoWidth:		false
+	};
+
+	/**
+	 * checkVideoLinks
+	 * @desc Check if for any videos links
+	 * @since 2.0.0
+	 */
+
+	Video.prototype.checkVideoLinks = function(){
+		var videoEl,item;
+
+		for(var i = 0; i<this.owl.num.items; i++){
+
+			item = this.owl.dom.$items.eq(i);
+			if(item.data('owl-item').hasVideo){
+				continue;
+			}
+
+			videoEl = item.find('.owl-video');
+			if(videoEl.length){
+				this.owl.state.hasVideos = true;
+				this.owl.dom.$items.eq(i).data('owl-item').hasVideo = true;
+				videoEl.css('display','none');
+				this.getVideoInfo(videoEl,item);
+			}
+		}
+	};
+
+	/**
+	 * getVideoInfo
+	 * @desc Get Video ID and Type (YouTube/Vimeo only)
+	 * @since 2.0.0
+	 */
+
+	Video.prototype.getVideoInfo = function(videoEl,item){
+
+		var info, type, id,
+			vimeoId = videoEl.data('vimeo-id'),
+			youTubeId = videoEl.data('youtube-id'),
+			width = videoEl.data('width') || this.owl.options.videoWidth,
+			height = videoEl.data('height') || this.owl.options.videoHeight,
+			url = videoEl.attr('href');
+
+		if(vimeoId){
+			type = 'vimeo';
+			id = vimeoId;
+		} else if(youTubeId){
+			type = 'youtube';
+			id = youTubeId;
+		} else if(url){
+			id = url.match(/(http:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/);
+			
+			if (id[3].indexOf('youtu') > -1) {
+				type = 'youtube';
+			} else if (id[3].indexOf('vimeo') > -1) {
+				type = 'vimeo';
+			}
+			id = id[6];
+		} else {
+			throw new Error('Missing video link.');
+		}
+
+		item.data('owl-item').videoType = type;
+		item.data('owl-item').videoId = id;
+		item.data('owl-item').videoWidth = width;
+		item.data('owl-item').videoHeight = height;
+
+		info = {
+			type: type,
+			id: id
+		};
+		
+		// Check dimensions
+		var dimensions = width && height ? 'style="width:'+width+'px;height:'+height+'px;"' : '';
+
+		// wrap video content into owl-video-wrapper div
+		videoEl.wrap('<div class="owl-video-wrapper"'+dimensions+'></div>');
+
+		this.createVideoTn(videoEl,info);
+	};
+
+	/**
+	 * createVideoTn
+	 * @desc Create Video Thumbnail
+	 * @since 2.0.0
+	 */
+
+	Video.prototype.createVideoTn = function(videoEl,info){
+
+		var tnLink,icon,height;
+		var customTn = videoEl.find('img');
+		var srcType = 'src';
+		var lazyClass = '';
+		var that = this.owl;
+
+		if(this.owl.options.lazyLoad){
+			srcType = 'data-src';
+			lazyClass = 'owl-lazy';
+		}
+
+		// Custom thumbnail
+
+		if(customTn.length){
+			addThumbnail(customTn.attr(srcType));
+			customTn.remove();
+			return false;
+		}
+		
+		function addThumbnail(tnPath){
+			icon = '<div class="owl-video-play-icon"></div>';
+
+			if(that.options.lazyLoad){
+				tnLink = '<div class="owl-video-tn '+ lazyClass +'" '+ srcType +'="'+ tnPath +'"></div>';
+			} else{
+				tnLink = '<div class="owl-video-tn" style="opacity:1;background-image:url(' + tnPath + ')"></div>';
+			}
+			videoEl.after(tnLink);
+			videoEl.after(icon);
+		}
+
+		if(info.type === 'youtube'){
+			var path = "http://img.youtube.com/vi/"+ info.id +"/hqdefault.jpg";
+			addThumbnail(path);
+		} else
+		if(info.type === 'vimeo'){
+			$.ajax({
+				type:'GET',
+				url: 'http://vimeo.com/api/v2/video/' + info.id + '.json',
+				jsonp: 'callback',
+				dataType: 'jsonp',
+				success: function(data){
+					var path = data[0].thumbnail_large;
+					addThumbnail(path);
+					if(that.options.loop){
+						that.updateItemState();
+					}
+				}
+			});
+		}
+	};
+
+	/**
+	 * stopVideo
+	 * @since 2.0.0
+	 */
+
+	Video.prototype.stopVideo = function(){
+		this.owl.trigger('videoStop');
+		var item = this.owl.dom.$items.eq(this.owl.state.videoPlayIndex);
+		item.find('.owl-video-frame').remove();
+		item.removeClass('owl-video-playing');
+		this.owl.state.videoPlay = false;
+	};
+
+	/**
+	 * playVideo
+	 * @since 2.0.0
+	 */
+
+	Video.prototype.playVideo = function(ev){
+		this.owl.trigger('videoPlay');
+
+		if(this.owl.state.videoPlay){
+			this.stopVideo();
+		}
+		var videoLink,videoWrap,
+			target = $(ev.target || ev.srcElement),
+			item = target.closest('.'+this.owl.options.itemClass);
+
+		var videoType = item.data('owl-item').videoType,
+			id = item.data('owl-item').videoId,
+			width = item.data('owl-item').videoWidth || Math.floor(item.data('owl-item').width - this.owl.options.margin),
+			height = item.data('owl-item').videoHeight || this.owl.dom.$stage.height();
+
+		if(videoType === 'youtube'){
+			videoLink = "<iframe width=\""+ width +"\" height=\""+ height +"\" src=\"http://www.youtube.com/embed/" + id + "?autoplay=1&v=" + id + "\" frameborder=\"0\" allowfullscreen></iframe>";
+		} else if(videoType === 'vimeo'){
+			videoLink = '<iframe src="http://player.vimeo.com/video/'+ id +'?autoplay=1" width="'+ width +'" height="'+ height +'" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+		}
+		
+		item.addClass('owl-video-playing');
+		this.owl.state.videoPlay = true;
+		this.owl.state.videoPlayIndex = item.data('owl-item').indexAbs;
+
+		videoWrap = $('<div style="height:'+ height +'px; width:'+ width +'px" class="owl-video-frame">' + videoLink + '</div>');
+		target.after(videoWrap);
+	};
+
+	Video.prototype.isInFullScreen = function(){
+
+		// if Vimeo Fullscreen mode
+		var fullscreenElement = document.fullscreenElement || document.mozFullScreenElement || document.webkitFullscreenElement;
+		if(fullscreenElement){
+			if($(fullscreenElement.parentNode).hasClass('owl-video-frame')){
+				this.owl.setSpeed(0);
+				this.owl.state.isFullScreen = true;
+			}
+		}
+
+		if(fullscreenElement && this.owl.state.isFullScreen && this.owl.state.videoPlay){
+			return false;
+		}
+
+		// Comming back from fullscreen
+		if(this.owl.state.isFullScreen){
+			this.owl.state.isFullScreen = false;
+			return false;
+		}
+
+		// check full screen mode and window orientation
+		if (this.owl.state.videoPlay) {
+			if(this.owl.state.orientation !== window.orientation){
+				this.owl.state.orientation = window.orientation;
+				return false;
+			}
+		}
+		return true;
+	};
+
+	Video.prototype.destroy = function(){
+		this.owl.dom.$el.off('.owl');
+		this.owl.dom.$el.off('.owl.video');
+	};
+
+	$.fn.owlCarousel.Constructor.Plugins.video = Video;
+
+})( window.Zepto || window.jQuery, window,  document );
