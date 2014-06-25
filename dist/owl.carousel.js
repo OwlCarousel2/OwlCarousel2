7c5c7863c1d7d4ce7f88241f2e304d3c5ddb84de
Artus Kolanowski <artus@ionoi.net>
2014-06-26 01:27:16 +0200

    Removes debug calls from navigation plugin

--- dist/owl.carousel.js
+++ dist/owl.carousel.js
@@ -2925,7 +2925,6 @@
 	 * @param {Number} [speed=false] - The time in milliseconds for the transition.
 	 */
 	Navigation.prototype.next = function(speed) {
-		console.log(this.getPosition(true));
 		$.proxy(this._overrides.to, this._core)(this.getPosition(true), speed);
 	}
 
@@ -2935,7 +2934,6 @@
 	 * @param {Number} [speed=false] - The time in milliseconds for the transition.
 	 */
 	Navigation.prototype.prev = function(speed) {
-		console.log(this.getPosition(false));
 		$.proxy(this._overrides.to, this._core)(this.getPosition(false), speed);
 	}
 
