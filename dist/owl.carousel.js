9da74b4f628673c58ba488ca8d61785e1fee6288
Alexandre Kilian <alexandre@pulpmedia.at>
2014-06-25 13:43:24 +0200

    Fix for slideBy = 'page' without dots
    Fixed a problem where pages where not being calculated if dots was
    inactive, but slideBy was set to „page“

--- dist/owl.carousel.js
+++ dist/owl.carousel.js
@@ -2812,7 +2812,7 @@
 			options.slideBy = Math.min(options.slideBy, options.items);
 		}
 
-		if (options.dots) {
+		if (options.dots || options.slideBy == 'page') {
 			this._pages = [];
 
 			for (i = lower, j = 0, k = 0; i < upper; i++) {
