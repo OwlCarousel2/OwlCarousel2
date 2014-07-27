d179d0b7bed0eff4ade3cdc3ba20b7dcaccd1e8f
Artus Kolanowski <artus@ionoi.net>
2014-07-27 02:39:57 +0200

    Corrects typo in items `active` and `center` classes

--- src/js/owl.carousel.js
+++ src/js/owl.carousel.js
@@ -381,12 +381,12 @@
 				}
 			}
 
-			this.$stage.children('.active').removeClass('.active');
-			this.$stage.children(':eq(' + matches.join('), :eq(') + ')').addClass('.active');
+			this.$stage.children('.active').removeClass('active');
+			this.$stage.children(':eq(' + matches.join('), :eq(') + ')').addClass('active');
 
 			if (this.settings.center) {
-				this.$stage.children('.center').removeClass('.center');
-				this.$stage.children().eq(this.current()).addClass('.center');
+				this.$stage.children('.center').removeClass('center');
+				this.$stage.children().eq(this.current()).addClass('center');
 			}
 		}
 	} ];
