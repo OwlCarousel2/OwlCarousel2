e8cf9c47d35534e62996b0d8a9518760bdedb748
Daniel Bacher <daniel@Daniels-iMac.local>
2014-07-28 10:00:24 +0200

    Autoplay buttons code update
     - Add defaults and posibility to change container class
     - Add text for buttons into settings (defaults)
     - Add class for buttons into settings (defaults)
     - Check on namespace
     - Format for coding standard

--- src/js/owl.autoplayButtons.js
+++ src/js/owl.autoplayButtons.js
@@ -5,69 +5,75 @@
  * @license The MIT License (MIT)
  *
  * HowTo:
- * set {buttons: true} in owl-carousel constructor
+ * set {autoplayButtons: true} in owl-carousel constructor
  * owl.autoplay.js must be included!
  */
 ;(function($, window, document, undefined) {
-    'use strict';
+	'use strict';
 
-    var AutoplayButtons = function(carousel) {
+	var AutoplayButtons = function(carousel) {
 
-        this._core = carousel;
-        this._initialized = false;
-        this.$element = this._core.$element;
+		this._core = carousel;
+		this.$element = this._core.$element;
 
-        this.controls = {};
+		// set default options
+		this._core.options = $.extend({}, AutoplayButtons.Defaults, this._core.options);
 
-        this.controls.$play = $('<a href="#" class="play">Play</a>');
-        this.controls.$stop = $('<a href="#" class="stop">Stop</a>');
+		this.controls = {};
+		this.controls.$play = $('<a href="#" class="'+this._core.options.buttonsClass[0]+'">'+this._core.options.buttonsText[0]+'</a>');
+		this.controls.$stop = $('<a href="#" class="'+this._core.options.buttonsClass[1]+'">'+this._core.options.buttonsText[1]+'</a>');
 
-        this.template = '<div class="controlButtons"></div>';
+		this.template = '<div class="'+this._core.options.buttonsContainerClass+'"></div>';
 
-        this._handlers = {
-            'initialize.owl.carousel': $.proxy(function(e) {
-                if(this._initialized) return;
+		this._handlers = {
+			'initialize.owl.carousel': $.proxy(function(e) {
+				if (!e.namespace) {
+					return;
+				}
 
-                var $template = $(this.template);
+				var $template = $(this.template);
 
-                $template.insertAfter(this.$element);
+				$template.insertAfter(this.$element);
 
-                $template.append(this.controls.$stop);
-                $template.append(this.controls.$play);
+				$template.append(this.controls.$stop);
+				$template.append(this.controls.$play);
 
-                this.controls.$stop.on('click', $.proxy(function(e){
-                    this.stop(this);
-                }, this));
+				this.controls.$stop.on('click', $.proxy(function(e){
+					this.stop(this);
+				}, this));
 
-                this.controls.$play.on('click', $.proxy(function(e){
-                    this.play(this);
-                }, this));
+				this.controls.$play.on('click', $.proxy(function(e){
+					this.play(this);
+				}, this));
 
-            }, this)
-        };
+			}, this)
+		};
 
-        // set default options
-        this._core.options = $.extend({}, AutoplayButtons.Defaults, this._core.options);
+		if(!this._core.options.autoplayButtons) return;
 
-        if(!this._core.options.buttons) return;
+		// register event handlers
+		this.$element.on(this._handlers);
+	};
 
-        // register event handlers
-        this.$element.on(this._handlers);
-    }
+	AutoplayButtons.Defaults = {
+		buttonsContainerClass: 'owl-autoplay-controls',
+		buttonsText: ['play', 'stop'],
+		buttonsClass: ['play', 'stop']
+	};
 
-    // Handle the play click
-    AutoplayButtons.prototype.play = function(ev) {
-        if(!this._core.settings.autoplay)
-            this._core.settings.autoplay = true;
+	// Handle the play click
+	AutoplayButtons.prototype.play = function(ev) {
+		if(!this._core.settings.autoplay)
+			this._core.settings.autoplay = true;
 
-        this.$element.trigger('play.autoplay.owl', [5000]);
-    }
+		this.$element.trigger('play.autoplay.owl', [5000]);
+	};
 
-    // Handle the stop click
-    AutoplayButtons.prototype.stop = function(ev) {
-        this.$element.trigger('stop.autoplay.owl');
-    }
+	// Handle the stop click
+	AutoplayButtons.prototype.stop = function(ev) {
+		this.$element.trigger('stop.autoplay.owl');
+	};
 
-    $.fn.owlCarousel.Constructor.Plugins.AutoplayButtons = AutoplayButtons;
+	$.fn.owlCarousel.Constructor.Plugins.AutoplayButtons = AutoplayButtons;
 
 })(window.Zepto || window.jQuery, window, document);
