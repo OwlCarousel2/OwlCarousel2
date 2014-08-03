cd9064c99461bc772e50a6a36c9b3d513571d1bb
Artus Kolanowski <artus@ionoi.net>
2014-08-03 23:29:36 +0200

    Corrects `draw` of navigation plugin

--- src/js/owl.navigation.js
+++ src/js/owl.navigation.js
@@ -154,14 +154,14 @@
 		this._controls.$relative = (settings.navContainer ? $(settings.navContainer)
 			: $('<div>').addClass(settings.navContainerClass).appendTo(this.$element)).addClass('disabled');
 
-		$('<' + settings.navElement + '>')
+		this._controls.$previous = $('<' + settings.navElement + '>')
 			.addClass(settings.navClass[0])
 			.html(settings.navText[0])
 			.prependTo(this._controls.$relative)
 			.on('click', $.proxy(function(e) {
 				this.prev(settings.navSpeed);
 			}, this));
-		$('<' + settings.navElement + '>')
+		this._controls.$next = $('<' + settings.navElement + '>')
 			.addClass(settings.navClass[1])
 			.html(settings.navText[1])
 			.appendTo(this._controls.$relative)
@@ -266,10 +266,8 @@
 		this._controls.$relative.toggleClass('disabled', !settings.nav || disabled);
 
 		if (settings.nav && !settings.loop && !settings.rewind) {
-			this._controls.$relative.find('.' + settings.navClass[0])
-				.toggleClass('disabled', index <= this._core.minimum(true));
-			this._controls.$relative.find('.' + settings.navClass[1])
-				.toggleClass('disabled', index >= this._core.maximum(true));
+			this._controls.$previous.toggleClass('disabled', index <= this._core.minimum(true));
+			this._controls.$next.toggleClass('disabled', index >= this._core.maximum(true));
 		}
 
 		this._controls.$absolute.toggleClass('disabled', !settings.dots || disabled);
