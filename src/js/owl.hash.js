9f5f8e677c9b5dd664a61e1d94af4a1571a233f9
Artus Kolanowski <artus@ionoi.net>
2014-08-05 05:16:08 +0200

    Changes update handler for `changed` and `hashchanged` in hash plugin
    Avoids multiple invocations of the event handlers. Uses `changed`
    instead of `translated` event to reduce the number of calls even when
    this seems to broke the animation on some Chrome devices.
    
    Refers #389

--- src/js/owl.hash.js
+++ src/js/owl.hash.js
@@ -21,7 +21,7 @@
 		this._core = carousel;
 
 		/**
-		 * Hash table for the hashes.
+		 * Hash index for the items.
 		 * @protected
 		 * @type {Object}
 		 */
@@ -40,26 +40,35 @@
 		 */
 		this._handlers = {
 			'initialized.owl.carousel': $.proxy(function(e) {
-				if (e.namespace && this._core.settings.startPosition == 'URLHash') {
+				if (e.namespace && this._core.settings.startPosition === 'URLHash') {
 					$(window).trigger('hashchange.owl.navigation');
 				}
 			}, this),
 			'prepared.owl.carousel': $.proxy(function(e) {
 				if (e.namespace) {
 					var hash = $(e.content).find('[data-hash]').andSelf('[data-hash]').attr('data-hash');
+
+					if (!hash) {
+						return;
+					}
+
 					this._hashes[hash] = e.content;
 				}
 			}, this),
-			'translated.owl.carousel': $.proxy(function(e) {
-				var hash = this._core.$stage
-					.children()
-					.eq(this._core.current())
-					.find('[data-hash]')
-					.attr('data-hash');
-				if (hash) {
+			'changed.owl.carousel': $.proxy(function(e) {
+				if (e.namespace && e.property.name === 'position') {
+					var current = this._core.items(this._core.relative(this._core.current())),
+						hash = $.map(this._hashes, function(item, hash) {
+							return item === current ? hash : null;
+						}).join();
+
+					if (!hash || window.location.hash.slice(1) === hash) {
+						return;
+					}
+
 					window.location.hash = hash;
 				}
-			})
+			}, this)
 		};
 
 		// set default options
@@ -69,13 +78,13 @@
 		this.$element.on(this._handlers);
 
 		// register event listener for hash navigation
-		$(window).on('hashchange.owl.navigation', $.proxy(function() {
+		$(window).on('hashchange.owl.navigation', $.proxy(function(e) {
 			var hash = window.location.hash.substring(1),
 				items = this._core.$stage.children(),
 				position = this._hashes[hash] && items.index(this._hashes[hash]);
 
-			if (position === undefined) {
-				return false;
+			if (position === undefined || position === this._core.current()) {
+				return;
 			}
 
 			this._core.to(this._core.relative(position), false, true);
