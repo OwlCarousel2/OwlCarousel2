c3f22a9a1e752f83254d579f3d2a764fa21f68dc
haqii <mail@haqii.com>
2014-09-03 16:46:30 +0800

    use `substring(0, 1)` instead of `[0]` because IE7 does not support the latter one.

--- src/js/owl.carousel.js
+++ src/js/owl.carousel.js
@@ -158,7 +158,7 @@
 		}, this));
 
 		$.each(Owl.Plugins, $.proxy(function(key, plugin) {
-			this._plugins[key[0].toLowerCase() + key.slice(1)]
+			this._plugins[key.substring(0, 1).toLowerCase() + key.slice(1)]
 				= new plugin(this);
 		}, this));
 
