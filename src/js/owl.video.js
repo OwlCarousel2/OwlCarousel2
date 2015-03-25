65016de7ac26c181f29603afc4304e9b3124c3eb
Emanuele Parisio <emanuele.parisio@soprasteria.com>
2015-03-25 17:48:30 +0100

    support custom parameter in youtube video urls
    add a basic support for youtube parameter in the url

--- src/js/owl.video.js
+++ src/js/owl.video.js
@@ -25,7 +25,7 @@
 		 * @type {Object}
 		 */
 		this._videos = {};
-
+		this._urlParameters = {};
 		/**
 		 * Current playing item.
 		 * @protected
@@ -113,6 +113,11 @@
 
 			if (id[3].indexOf('youtu') > -1) {
 				type = 'youtube';
+
+				var splittedUrl = url.split(/\?/),
+		        	params = splittedUrl[1] ? splittedUrl[1] : null;
+		        this._urlParameters[url] = params;
+
 			} else if (id[3].indexOf('vimeo') > -1) {
 				type = 'vimeo';
 			} else {
@@ -217,6 +222,7 @@
 		var target = $(event.target),
 			item = target.closest('.' + this._core.settings.itemClass),
 			video = this._videos[item.attr('data-video')],
+			params = this._urlParameters[item.attr('data-video')],
 			width = video.width || '100%',
 			height = video.height || this._core.$stage.height(),
 			html;
@@ -233,8 +239,13 @@
 		this._core.reset(item.index());
 
 		if (video.type === 'youtube') {
-			html = '<iframe width="' + width + '" height="' + height + '" src="http://www.youtube.com/embed/' +
+			if(params){
+				html = '<iframe width="' + width + '" height="' + height + '" src="http://www.youtube.com/embed/' +
+				video.id + '?autoplay=1&' + params + '" frameborder="0" allowfullscreen></iframe>';
+			}else{
+				html = '<iframe width="' + width + '" height="' + height + '" src="http://www.youtube.com/embed/' +
 				video.id + '?autoplay=1&v=' + video.id + '" frameborder="0" allowfullscreen></iframe>';
+			}
 		} else if (video.type === 'vimeo') {
 			html = '<iframe src="http://player.vimeo.com/video/' + video.id +
 				'?autoplay=1" width="' + width + '" height="' + height +
