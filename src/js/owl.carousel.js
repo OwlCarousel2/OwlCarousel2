366d7deb6d4fa39742a169fb0b78e7dcf54a81c2
Tom Hopcraft <tomhop88@btinternet.com>
2016-03-10 17:58:32 +0000

    [feature #981] - add css easing option

--- src/js/owl.carousel.js
+++ src/js/owl.carousel.js
@@ -207,6 +207,7 @@
 		responsiveBaseElement: window,
 
 		fallbackEasing: 'swing',
+		cssEasing: 'linear',
 
 		info: false,
 
@@ -880,7 +881,7 @@
 		if ($.support.transform3d && $.support.transition) {
 			this.$stage.css({
 				transform: 'translate3d(' + coordinate + 'px,0px,0px)',
-				transition: (this.speed() / 1000) + 's'
+				transition: (this.speed() / 1000) + 's ' + this.settings.cssEasing
 			});
 		} else if (animate) {
 			this.$stage.animate({
